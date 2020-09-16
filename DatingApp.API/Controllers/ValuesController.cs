6f2bbbb12d39569afda7752c60f3390127892df1
unknown <pattariya.th@mail.kmutt.ac.th>
2020-09-16 22:22:53 +0700

    End of Section 3

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Controllers
 {
     //http:localhost:5000/api/values      values call ValuesController
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
@@ -34,6 +36,7 @@ namespace DatingApp.API.Controllers
         // GET api/values/5
         //get record
         //อันนี้จะ return คำว่า values ออกไปถ้ามีการ /id คือมีการ /ตัวเลขตามหลัง api/values ไป
+        [AllowAnonymous]
         [HttpGet("{id}")]
         public async Task<IActionResult> GetValue(int id)
         {
